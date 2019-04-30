80fe1b9dd1325ee79a23a0486dc9a48060deb531
eliagipe <elia@gilgou.com>
2019-04-30 13:02:38 +0200

    elia - validate.php

---
+++ validate.php
@@ -0,0 +1,21 @@
+<?php 
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    //Connect to DB
+
+    $db = new mysqli('localhost', 'root', 'root', 'website');
+    $query = " SELECT * FROM users WHERE username = '$email' AND password = '$password' ";
+    $result = mysqli_query($db, $query);
+    $rows = mysqli_num_rows($result);
+
+    if ($rows > 0) {
+        header("location:search.html");
+    } else {
+        echo "Try again.";
+    }
+
+    mysqli_free_result($result);
+    mysqli_close($db);
+
+?>
