f0f636a2c0cb8aed8fae9c37cf17e3dcbfbdf5f5
eliagipe <elia@gilgou.com>
2019-05-01 11:18:09 +0200

    ultim

--- aboutus.php
+++ aboutus.php
@@ -1,26 +1,88 @@
-<?php include_once 'includes/templates/header.php'; ?>
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans|Oswald|PT+Sans" rel="stylesheet">
+
+  <meta name="theme-color" content="#fafafa">
+</head>
+
+
+<body>
+  <!--[if IE]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
+
+  <!-- Add your site or application content here -->
+
+<header class= "bar">
+  <div class= "hero">  
+      <div class="conteiner clearfix"> <!--This is to center what we are going to write-->
+        <div class="welcome">
+          <a href="index.php"><img src="img/HomeStay logo.png" alt="Logo"></a>
+        </div>
+
+        <div class="menu-movil">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        <nav class="principal-navegation"> <!--this class is made so then this icons does not mix with others-->
+          <?php 
+            if ($_SESSION["type"] == null) {
+              echo "<a href='signin.php'>Sign In</a>";
+            } else {
+              echo "<a href='logout.php'>Sign Out</a>";
+            }
+          ?>
+
+          <!-- TODO falta versió estudiants i famílies -->
+          <?php if($_SESSION["type"] = 'F') {
+              echo "<a href='profile.php'>My profile</a>";
+              echo "<a href='search.php'>Search match</a>";
+              echo "<a href='myfavorits.php'>Favorites</a>";
+          } ?>
+          <a href="aboutus.php">About us</a>
+          <a href="faq.php">FAQ</a>
+        </nav>
+      </div><!--.conteneiner-->
+  </div><!--.bar-->
+</header>
 
 <section class="section conteiner">
     <h2>About us</h2>
-<div class ="About">   
-     
-<br>We are helping danish families and young international students  to find each other since 2019.</br>
+    <div class ="About">   
+        <br>We are helping danish families and young international students  to find each other since 2019.<br>
 
-<br>We administer this website from the Computer Science department from Aalborg University</br>
-<br> which is located in the main campus close to the new University Hospital. </br> <br>
-<br> There are several ways to come in contact with us:</br>
-<br>If you have any questions you are welcome to come visit us in the Computer Science department.</br>
-<br> you can alsp call us or write us an email. </br>
+        <br>We administer this website from the Computer Science department from Aalborg University<br>
+        <br> which is located in the main campus close to the new University Hospital. <br>
+        <br> There are several ways to come in contact with us:<br>
+        <br>If you have any questions you are welcome to come visit us in the Computer Science department.<br>
+        <br> you can alsp call us or write us an email. <br>
 
-<div class ="about">
-Phone number:</div>
-<br>+45 22 34 34 00 34</br>
-<div class="about">E-mail:</div>
-<br>admin@homeStay.dk</br>
-<div class="about">Address:</div>
-<br>Selma Lagerløfs Vej 300, 9220 Aalborg</br>
-</div> 
+        <div class ="about">
+        Phone number:</div>
+        <br>+45 22 34 34 00 34<br>
+        <div class="about">E-mail:</div>
+        <br>admin@homeStay.dk<br>
+        <div class="about">Address:</div>
+        <br>Selma Lagerløfs Vej 300, 9220 Aalborg<br>
+    </div> 
 </section>
+
 <div id="map" class="map"> </div>
 
 <?php include_once 'includes/templates/footer.php'; ?>
