428564ba35b15ad9f9af922d94b3e0dcecce42e2
eliagipe <elia@gilgou.com>
2019-05-06 11:24:49 +0200

    correct profiles

--- js/main.js
+++ js/main.js
@@ -45,9 +45,10 @@ $('body').css({'margin-top':'0px'});
 $('.menu-movil') .on('click',function(){
 $(".navegacion-principal").slideToggle();//cuando esteen slide dog ejecutara down y asi.
 
-});
+
 
     });// DOM CONTENT LOADED
+
 
     $('body.signin .principal-navegation a:contains("Sign In")').addClass('active');
     $('body.profileS .principal-navegation a:contains("My profile")').addClass('active');
