595ee4a58ab732c71ba22c41ba93630709407869
Aparna Ramachandram <aparna.r@outlook.dk>
2019-05-06 12:21:38 +0200

    somethign

--- profile.php
+++ profile.php
@@ -268,7 +268,7 @@
             <input type="file" id="real-input" name="real-input">
             <button class="browse-btn">Browse Files</button>
             <span class="file-info">Upload your criminal record</span>
-            <p><a href="save.html" class="long-button hollow"> Upload criminal record</a></p>     
+            <p><a href="profiledb.php" class="long-button hollow"> Upload criminal record</a></p>     
           </div>
         </div>
       </div>
@@ -291,51 +291,4 @@
 </section>
 </form>
 
-<footer class="site-footer">
-  <div class="conteiner clearfix">
-    <div class="footer-informacion">
-      <h3>About <span>  HomeStay</span></h3> <!--so that about is in one colour and homestay in another -->
-      <p>The best website to help match exchange students from AAU with host families.</p>
-    </div>
-    <div class="last-tewts">
-      <h3>Our last <span> tweets</span></h3>
-      <ul>
-        <li>Yet another satisfied student with our webpage!</li>
-          <li>Remember that our Homestay webpage is 100% FREE!</li>
-          <li>120 students & 80 families have used our website so far and 89% is still in contact!</li>
-      </ul>
-    </div>
-    <div class="menu">
-      <h3>Social <span> networks:</span></h3>
-      <nav class= "social-networks">
-        <a href="#"><i class="fab fa-facebook-f"></i></a>
-        <a href="#"><i class="fab fa-twitter-square"></i></a>
-        <a href="#"><i class="fab fa-pinterest"></i></a>
-        <a href="#"><i class="fab fa-youtube-square"></i></a>
-        <a href="#"><i class="fab fa-instagram"></i></a>
-      </nav>
-    </div>
-  </div>
-  <p class="copyright">
-    All rights reserved HomeStay 2019.
-  </p>
-</footer>
-
-
-
- <!--Here finishHTML HOME STAY-->
-  <script src="js/vendor/modernizr-3.7.1.min.js"></script>
-  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-  <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
-  <script src="js/plugins.js"></script>
-  <script src="js/main.js"></script>
-
-  <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
-  <script>
-    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
-    ga('create', 'UA-XXXXX-Y', 'auto'); ga('set','transport','beacon'); ga('send', 'pageview')
-  </script>
-  <script src="https://www.google-analytics.com/analytics.js" async defer></script>
-</body>
-
-</html>;
+<?php include_once "includes/templates/footer.php"; ?>
