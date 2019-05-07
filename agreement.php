075dc0162de20057bd5f777afc1e1247f7e4c677
eliagipe <elia@gilgou.com>
2019-05-07 19:04:11 +0200

    agreement

---
+++ agreement.php
@@ -0,0 +1,15 @@
+<?php include_once 'includes/templates/header.php'; ?>
+
+<section class="section conteneiner">
+  <h2>You are about making an agreement yeeey</h2>
+  <p>You are about making an agreement with (name of profile in php code)</p>
+  <p>We suggest you to follow the next instructions:</p>
+  <p>Make a paper where you agree on the duration of the stay, the mutual rules</p>
+  <p>the price, the expectations of this experience and the most important:</p>   
+  <p>Remember to sign it! when you will have everything ready then press the following boton, which</p>
+  <p> is going to deactivate  both profiles. so is important that you are sure about your decision</p>
+  <a href="agreement_done.html" class="button hollow">Confirm</a>
+  <a href="index.php" class="button hollow">Cancel</a>
+</section>
+
+<?php include_once 'includes/templates/footer.php'; ?>
