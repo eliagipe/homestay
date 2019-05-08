0afc9f805e7e75d92c8181b37512054dee2fcf25
Natatass <gataclow@hotmail.com>
2019-05-08 08:12:59 +0200

    favorites

---
+++ gaturu.php
@@ -0,0 +1,17 @@
+<?php 
+session_start();
+$RegisterId = $_SESSION["RegisterId"];
+
+$fav=$_GET['fav'];
+echo $fav; 
+
+
+require_once('connectiondb.php');
+$stmt = $db->prepare(" INSERT INTO account_favs (registeredid, favid)VALUES ($RegisterId, $fav) ");
+//$stmt->bind_param("sssss", $firstname, $lastname, $email, $passwordEncrypted, $type);
+$stmt->execute();
+$stmt->close();
+
+
+
+?>
--- myfavorits.php
+++ myfavorits.php
@@ -1,9 +1,80 @@
-<?php session_start(); ?>
+<?php session_start();  require_once('connectiondb.php');?>
 
-<?php include_once 'includes/templates/header.php'; ?>
+
+<?php include_once 'includes/templates/header.php';
+$RegisterId = $_SESSION["RegisterId"];
+
+
+
+$query = "SELECT * 
+        FROM account_favs
+        WHERE registeredid = $RegisterId
+        ";
+
+$result = mysqli_query($db, $query);
+$rows = mysqli_num_rows($result);
+
+?>
 
 <section class="section conteiner">
     <h2>My favorites</h2>
+
+    <div class="profiles conteiner clearfix">
+      <div class="profile">
+     
+        <?php while($rows=$result->fetch_object()) { ?>
+          <blockquote class="search-result grid-container">
+
+            <div class="grid-item item1">
+              <img src="img/Loui 23.jpg" alt="student">
+              <p class="favorite">Mark as favorite: <a href="gaturu.php?fav=<?php echo $rows->registeredid ?>"><i class="far fa-heart"></i></a> </p>
+            </div>
+
+         <?php   $query2 = "SELECT * 
+        FROM account_register
+        WHERE RegisterId = $rows->registeredid
+        ";
+
+$result2 = mysqli_query($db, $query2);
+$rows2 = mysqli_fetch_array($result2);
+
+
+
+echo "estoy botando este dato". $rows2["FirstName"]."<br>";
+
+$query3 = "SELECT * 
+FROM student
+WHERE RegisterIdS = $rows->registeredid
+";
+
+$result3 = mysqli_query($db, $query3);
+$rows3 = mysqli_fetch_array($result3);
+
+?>
+
+            <div class="grid-item item2">
+              <p class="name"><?php echo $rows2["FirstName"] . " " . $rows2["LastName"]; ?></p>
+              <p class="info"><span>Nationality:</span> <?php echo $rows3["Nationality"]; ?></p>
+              <p class="info"><span>Age:</span> <?php echo $rows3["Age"]; ?> years old</p>
+              <p class="info"><span>From:</span> <?php echo $rows3["AvailableFrom"]; ?></p>
+              <p class="info"><span>To:</span> <?php echo $rows3["AvailableTo"]; ?></p>
+            </div>
+
+            <div class="item3">
+              <a href="seeProfileS.php?student=<?php echo $rows3["RegisterIdS"] ?>" class="long-button hollow">See profile</a>
+            </div>
+
+          </blockquote>
+        <?php } ?>
+     
+
+      </div>
+  </div>
+
+
+
+
+
 
       <div class="profiles conteiner clearfix">
         <div class="profile">
